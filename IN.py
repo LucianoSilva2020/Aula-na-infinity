6c9982f416712995105dbe1a4e2ae86268c2744f
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-20 20:19:51 -0300

    ..

--- IN.py
+++ IN.py
@@ -5,13 +5,6 @@
 #---------------------------------
 # for var in range(7, 0, -2):
 #     print(var)
-#------------------------------------------
-# num = int(input('Digite um numero:'))
-
-# print(f'A tabuada de {num} é...')
-
-# for resultado in range(1, 101):
-#     print(f'{num} x {resultado:2} = {num * resultado}')
 #-------------------------------------------------------
 
 start = int(input('Digite o inicio do for:'))

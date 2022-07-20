1a327f641051c7a78a1d68a57536b01cb35b0f29
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-20 20:05:39 -0300

    ..

---
+++ IN.py
@@ -0,0 +1,23 @@
+# for var in range(0,10):
+#     print('Ola,mundo!')
+
+# print('Saiu do laço')   
+#---------------------------------
+# for var in range(7, 0, -2):
+#     print(var)
+#------------------------------------------
+# num = int(input('Digite um numero:'))
+
+# print(f'A tabuada de {num} é...')
+
+# for resultado in range(1, 101):
+#     print(f'{num} x {resultado:2} = {num * resultado}')
+#-------------------------------------------------------
+
+start = int(input('Digite o inicio do for:'))
+end = int(input('Digite o final do for:'))
+step = int(input('Digite o passo:'))
+
+for var in range(start, end, step):
+    print(var, end)
+print('Saiu do laço')    
